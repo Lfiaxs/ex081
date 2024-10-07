0b49e1d0ed61efbde9345a57abea644e8fb0a716
Lucas Fiais <lucas.fiais@hotmail.com>
2024-10-07 13:41:34 -0300

    Add files via upload

---
+++ ex081.py
@@ -0,0 +1,15 @@
+valores = []
+while True:
+    valores.append(int(input('Digite um número: ')))
+    resp = str(input('Deseja contiuar? [S/N]'))
+    if resp in 'Nn':
+        break
+print('-='*30)
+print(f'Você digitou {len(valores)} elemnetos.')
+valores.sort(reverse=True)
+print(f'Os valores em ordem decrescente são {valores}')
+if 5 in valores:
+    print('O valor 5 faz parte da lista!')
+else:
+    print('O valor 5 não foi encontrado na lista!')
+
